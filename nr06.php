81a585c14ee9f150b9092eed5b6555481bb9e307
dsdanielsodre <dsdanielsodre@gmail.com>
2022-06-15 18:04:49 -0300

    init

---
+++ nr06.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+   <head>
+      <?php
+         include 'links.php';
+         include 'metas.php';
+         include 'scripts.php';
+         ?>
+      <!-- MAPS -->
+      <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBQAx6CLr0p1zQFDhB-5lHxn_8X49pkAxE&callback=initMap"></script>
+      <!-- MAPS -->
+      <title>Grupo DM Serviços</title>
+   </head>
+   <body>
+      <?php
+         include "menu.php";
+
+         ?>
+      <main role="main">
+        <!--MARKETING-->
+         <div class="container marketing">
+           <div class="row">
+           <!--CONTEUDO SECUNDARIO -->
+            <div class="conteudo-secundario">
+               <h2 class="featurette-heading golden">NR-6<span class="text-muted"></h2>
+               <h4>Treinamento de uso correto de EPI (equipamento de proteção individual), ministrado pelo
+                 Técnico de Segurança do Trabalho. O treinamento será realizado no respectivo setor de trabalho, abordando os seguintes assuntos: <br>
+                <br>  1.	Conceito legal; <br>
+                <br>  2.	Uso correto; <br>
+                <br>  3.	Quais os EPIs que deverão ser utilizados pelos empregados das empresas ou  condomínio de acordo com as atividades realizadas; <br>
+                <br>  4.	Principais riscos; <br>
+                <br>  5.	Higienização e conservação dos EPIs; <br>
+                <br>  6.	Ficha de controle de entrega de EPIs; <br>
+                <br>  7.	Períodos de substituição dos EPIs. <br> <br>
+
+               </h4>
+
+            </div>
+         </div>
+         </div>
+         <!--/MARKETING-->
+         <!--/CONTEUDO SECUNDARIO -->
+
+         <!-- FOOTER -->
+         <?php
+            include "footer2.php";
+            ?>
+        <!-- /FOOTER -->
+
+      </main>
+   </body>
+</html>
